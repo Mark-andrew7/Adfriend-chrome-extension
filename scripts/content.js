07e07218b56c05bc24e73380d71266eda66fa72e
Mark-andrew7 <kamaumark19@gmail.com>
2025-02-04 08:41:11 +0000

    Fixed syntax error

--- scripts/content.js
+++ scripts/content.js
@@ -1,6 +1,6 @@
 const ads = document.querySelectorAll('div[class*="ad"], iframe[src*="ads"]');
 ads.forEach(ad => {
     const quote = document.createElement('div');
-    quote.textContent = "Stay posiive, you got this!"
+    quote.textContent = "Stay positive, you got this!"
     ad.replaceWith(quote);
 });
